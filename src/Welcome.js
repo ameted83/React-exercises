e63af3b6372f122f36935726246dc4c18e8a8e94
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-11 12:53:09 +0200

    props-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+const name = "Amelia";
 class Welcome extends React.Component {
   render() {
     return (
       <>
-        <p> Welcome, {this.props.name}!</p>
+        <p>
+          Welcome, <strong>{this.props.name}</strong>!
+        </p>
         <p>Your age is {this.props.age}</p>
       </>
     );
