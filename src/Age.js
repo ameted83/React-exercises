9273ff0a65b397377caa57ff201d8d0ddbf72b95
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-12 11:53:30 +0200

    conditional-rendering-04

--- src/Age.js
+++ src/Age.js
@@ -4,8 +4,13 @@ class Age extends React.Component {
   render() {
     return (
       <>
-        {this.props.age > 18 && this.props.age < 65 ? (
-          <p>"Your age is: {this.props.age}"</p>
+        {this.props.age > 18 &&
+        this.props.age < 65 &&
+        this.props.name === "John" ? (
+          <div>
+            <p>Name is: {this.props.name}</p>
+            <p>Your age is: {this.props.age}</p>
+          </div>
         ) : (
           <p></p>
         )}
