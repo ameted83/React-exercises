a63c75baefb4236ea4295f4b84cf3eb70d992e41
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-12 12:53:34 +0200

    state-04

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CounterDisplay from "./CounterDisplay";
 
 class Counter extends React.Component {
   state = {
@@ -18,7 +19,7 @@ class Counter extends React.Component {
   render() {
     return (
       <div>
-        <h1>Count: {this.state.count}</h1>
+        <CounterDisplay count={this.state.count} />
       </div>
     );
   }
