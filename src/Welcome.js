df7279d1d8c5b372ac6f97920c6fa5376d8cc875
Amelia Tedeschi <93825019+ameted83@users.noreply.github.com>
2022-05-14 12:23:17 +0200

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,11 @@
 import React from "react";
 class Welcome extends React.Component {
   render() {
-    const name=<strong>{this.props.name}</strong>
+
     return (
       <>
         <p>
-          Welcome, {name}!
+          Welcome, <strong>{this.props.name}</strong>!
         </p>
         <p>Your age is {this.props.age}</p>
       </>
