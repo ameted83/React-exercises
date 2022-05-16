2a87b4c7c76a112a446c3f6822dbb0fabcbeed38
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-16 15:39:17 +0200

    correct conditional-rendering-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends React.Component {
           Welcome, <strong>{this.props.name}</strong>!
         </p>
         {/* <p>Your age is: {this.props.age}</p> */}
-        <Age age={this.props.age ? this.props.age : false} />
+        {this.props.age && <Age age={this.props.age} />}
       </>
     );
   }
