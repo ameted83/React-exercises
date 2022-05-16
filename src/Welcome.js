a595ac8db8c6231964dd8463e55095739d4cf9aa
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-16 17:30:42 +0200

    styling-components-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,13 +3,13 @@ import Age from "./Age";
 class Welcome extends React.Component {
   render() {
     return (
-      <>
+      <div className="welcome">
         <p>
           Welcome, <strong>{this.props.name}</strong>!
         </p>
         {/* <p>Your age is: {this.props.age}</p> */}
         <Age age={this.props.age ? this.props.age : false} />
-      </>
+      </div>
     );
   }
 }
