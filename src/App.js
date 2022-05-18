8c49f570d681f4d82c20b99e7895c25a99bc1f89
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-18 19:00:00 +0200

    useEffect-02

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Hello from "./Hello";
 import Counter from "./Counter";
 import ClickCounter from "./ClickCounter";
@@ -8,68 +8,71 @@ import Login from "./Login";
 import { UncontrolLogin } from "./UncontrolLogin";
 import { TodoList } from "./TodoList";
 import { Container } from "./Container";
-import { DisplayLanguage } from "./DisplayLanguage";
+// import { DisplayLanguage } from "./DisplayLanguage";
 import { CreateLanguage } from "./CreateLanguage";
 import Sum from "./Sum";
 
-export class App extends React.Component {
-  state = {
-    language: "en",
-  };
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
-  render() {
-    return (
-      <>
-        <select
+export function App() {
+  // state = {
+  //   language: "en",
+  // };
+  // handleLanguageChange = (event) => {
+  //   this.setState({
+  //     language: event.target.value,
+  //   });
+  // };
+
+  const [showCounter, setShowCounter] = useState(true);
+  function handleToggleCounter() {
+    setShowCounter((s) => !s);
+  }
+
+  return (
+    <>
+      {/* <select
           value={this.state.language}
           onChange={this.handleLanguageChange}
         >
           <option value="en">English</option>
           <option value="it">Italiano</option>
-        </select>
-        <DisplayLanguage.Provider value={this.state.language}>
-          <Container title={"My application"}>
-            <CreateLanguage />
-            <Hello />
-            <InteractiveWelcome />
-            <Counter />
-            <ClickCounter />
-            <ClickTracker />
-            <Login />
-            <UncontrolLogin />
-            <TodoList
-              render={(todos, addItem, reset, removeItem) => {
-                return (
-                  <div>
-                    <form onSubmit={addItem}>
-                      <input name="todo"></input>
-                      <ul>
-                        {todos.map((item, index) => {
-                          return (
-                            <li>
-                              {item},
-                              <button onClick={() => removeItem(index)}>
-                                X
-                              </button>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                      <button type="submit">Invio</button>
-                      <button onClick={reset}>Reset</button>
-                    </form>
-                  </div>
-                );
-              }}
-            />
-          </Container>
-          <Sum />
-        </DisplayLanguage.Provider>
-      </>
-    );
-  }
+        </select> */}
+      {/* <DisplayLanguage.Provider value={this.state.language}> */}
+      <Container title={"My application"}>
+        <CreateLanguage />
+        <Hello />
+        <InteractiveWelcome />
+        <ClickCounter />
+        <ClickTracker />
+        <Login />
+        <UncontrolLogin />
+        <TodoList
+          render={(todos, addItem, reset, removeItem) => {
+            return (
+              <div>
+                <form onSubmit={addItem}>
+                  <input name="todo"></input>
+                  <ul>
+                    {todos.map((item, index) => {
+                      return (
+                        <li>
+                          {item},
+                          <button onClick={() => removeItem(index)}>X</button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <button type="submit">Invio</button>
+                  <button onClick={reset}>Reset</button>
+                </form>
+              </div>
+            );
+          }}
+        />
+        <Sum />
+        {showCounter && <Counter />}
+        <button onClick={handleToggleCounter}>Toggle Counter</button>
+      </Container>
+      {/* </DisplayLanguage.Provider> */}
+    </>
+  );
 }
