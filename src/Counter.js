66a5418b98ab959c4fe881acd70735a32e99d6bd
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-19 14:34:53 +0200

    custom-hooks-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 // import CounterDisplay from "./CounterDisplay";
 
-const Counter = ({ initialValue = 0 }) => {
+function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
-
-  useEffect(() => {
-    console.log(`The counter is now ${counter}`);
-  }, [counter]);
 
   function onCounterChange() {
     setCounter(counter + 1);
   }
+
+  function onCounterDecrement() {
+    setCounter((c) => c - 1);
+  }
+
+  function onCounterReset() {
+    setCounter(initialValue);
+  }
+
+  return {
+    counter: counter,
+    onIncrement: onCounterChange,
+    onDecrement: onCounterDecrement,
+    onReset: onCounterReset,
+  };
+}
+
+const Counter = ({ initialValue = 0 }) => {
+  const { counter, onIncrement, onDecrement, onReset } =
+    useCounter(initialValue);
+
   return (
     <div>
       <h2>Counter: {counter}</h2>
-      <button onClick={onCounterChange}>Click</button>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      <button onClick={onReset}>Reset</button>
       {/* <CounterDisplay count={this.state.count} /> */}
     </div>
   );
