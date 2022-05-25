030f827f1f9a93164307e5d04d9ced1f8324d898
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-25 18:16:02 +0200

    swr-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -3,15 +3,15 @@ import useGithubUser from "./UseGithubUser";
 
 const GithubUser = () => {
   const { username } = useParams();
-  const { data, loading, error } = useGithubUser(username);
+  const { data, loading, error, onFetchUser } = useGithubUser(username);
 
-  // function handleGetUserData() {
-  //   onFetchUser();
-  // }
+  function handleGetUserData() {
+    onFetchUser(username);
+  }
 
   return (
     <div>
-      {/* <button onClick={handleGetUserData}> Load</button> */}
+      <button onClick={handleGetUserData}> Load</button>
       {loading && <h2>Loading...</h2>}
       {error && <h2>There is an error</h2>}
       {data && <h1>{data.name}</h1>}
--- src/UseGithubUser.js
+++ src/UseGithubUser.js
@@ -2,11 +2,20 @@ import useSWR from "swr";
 import { Link, Outlet } from "react-router-dom";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
+
 function useGithubUser() {
-  const { data, error } = useSWR(`https://api.github.com/users`, fetcher);
+  const { data, error, mutate } = useSWR(
+    `https://api.github.com/users`,
+    fetcher
+  );
+
+  function handleRefreshUsers() {
+    mutate();
+  }
 
   return (
     <>
+      <button onClick={handleRefreshUsers}>Refresh</button>
       {!data && !error && <h3>Loading....</h3>}
       {error && <h3>An error has occurred</h3>}
       {data && !error && (
@@ -18,6 +27,7 @@ function useGithubUser() {
           ))}
         </ul>
       )}
+
       <Outlet />
     </>
   );
